0c22a6130eca9d55ec40e3dd76af792aed6a2b3d
AMarkInTheDark <127246522+AMarkInTheDark@users.noreply.github.com>
2026-04-28 16:35:20 -0600

    Add files via upload

---
+++ ModMetadata.cs
@@ -0,0 +1,18 @@
+using SPTarkov.Server.Core.Models.Spt.Mod;
+
+namespace RagmanClothingRebalanced;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.amarkinthedark.ragmanclothingrebalanced";
+    public override string Name { get; init; } = "RagmanClothingRebalanced";
+    public override string Author { get; init; } = "AMarkInTheDark (Marrko)";
+    public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.13");
+    public override string License { get; init; } = "MIT";
+    public override bool? IsBundleMod { get; init; } = false;
+    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = null;
+    public override string? Url { get; init; } = null;
+    public override List<string>? Contributors { get; init; } = null;
+    public override List<string>? Incompatibilities { get; init; } = null;
+}
---
+++ RagmanClothingRebalanced.cs
@@ -0,0 +1,38 @@
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Services;
+
+namespace RagmanClothingRebalanced;
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 3)]
+public class RagmanClothingRebalanced(
+    DatabaseService databaseService
+) : IOnLoad
+{
+    public async Task OnLoad()
+    {
+        var suits = databaseService.GetTables().Traders["5ac3b934156ae10c4430e83c"].Suits;
+
+        foreach (var suit in suits)
+        {
+            if (suit.SuiteId == "66043a3910bcdf80ff0e9f7c")
+            {
+                suit.Requirements.LoyaltyLevel = 1;
+                suit.Requirements.ProfileLevel = 8;
+                suit.Requirements.QuestRequirements = ["657315e4a6af4ab4b50f3459"];
+                suit.Requirements.AchievementRequirements = [];
+                suit.Requirements.ItemRequirements = [new() { Count = 64000, Tpl = "5449016a4bdc2d6f028b456f", OnlyFunctional = true, Type = "ItemRequirement" }];
+            }
+            else if (suit.SuiteId == "66043a8dc8949a435906e42c")
+            {
+                suit.Requirements.LoyaltyLevel = 1;
+                suit.Requirements.ProfileLevel = 8;
+                suit.Requirements.QuestRequirements = ["657315e4a6af4ab4b50f3459"];
+                suit.Requirements.AchievementRequirements = [];
+                suit.Requirements.ItemRequirements = [new() { Count = 48000, Tpl = "5449016a4bdc2d6f028b456f", OnlyFunctional = true, Type = "ItemRequirement" }];
+            }
+        }
+
+        await Task.CompletedTask;
+    }
+}
